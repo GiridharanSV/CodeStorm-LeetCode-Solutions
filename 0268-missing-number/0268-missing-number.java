d1a443093a7dce4840ce46130aa2e44a043a25ff
GIRIDHARAN S V <166196756+GiridharanSV@users.noreply.github.com>
2025-10-02 23:22:57 +0530

    Time: 0 ms (100%), Space: 45.7 MB (22.17%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
-      int missing=0;
-	    List<Integer>list=new ArrayList<>();
-	    for(int i=0;i<nums.length;i++){
-	        list.add(nums[i]);
-	    }
-	    for(int i=0;i<=nums.length;i++){
-	        if(!list.contains(i)){
-	            return i;
-	        }
-	    }
-	    return -1;  
+      int n = nums.length;
+        int xor = 0;
+        for (int i = 0; i <= n; i++) {
+            xor ^= i;
+        }
+        for (int num : nums) {
+            xor ^= num;
+        }
+        return xor;
     }
 }
