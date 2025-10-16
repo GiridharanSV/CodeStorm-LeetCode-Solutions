0cb422108edf95bd7cc60f111c9e3161f65a967c
GIRIDHARAN S V <166196756+GiridharanSV@users.noreply.github.com>
2025-10-16 18:13:18 +0530

    Time: 75 ms (43.49%), Space: 45.3 MB (59.13%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.java
@@ -1,7 +1,7 @@
 class Solution {
     public boolean repeatedSubstringPattern(String s) {
         int size = s.length();
-        String sFold = s.substring(1, size) + s.substring(0, size-1);
-        return sFold.contains(s);
+        String s1 = s.substring(1, size) + s.substring(0, size-1);
+        return s1.contains(s);
     }
 }
