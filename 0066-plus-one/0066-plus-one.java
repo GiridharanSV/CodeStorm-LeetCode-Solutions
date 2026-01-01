a09eaaef37b0833623c90253ff960f0753a7ea72
GIRIDHARAN S V <166196756+GiridharanSV@users.noreply.github.com>
2026-01-01 23:12:38 +0530

    Time: 0 ms (100%), Space: 43.4 MB (37.47%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+       int c=digits.length-1;
+      while(c>=0){
+digits[c]=digits[c]+1;
+if(digits[c]<10){
+    return digits;
+}
+digits[c]=0;
+c--;
+      }
+int[] arr=new int[digits.length+1];
+arr[0]=1;
+return arr;
+    }
+}
